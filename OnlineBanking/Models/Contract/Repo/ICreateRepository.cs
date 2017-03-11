608be56a73a532146635341ec3105a15c31d5e1c
Dmitriy-17 <lopushansky.d@gmail.com>
2017-03-11 20:05:21 +0200

    add IRepo

---
+++ OnlineBanking/Models/Contract/Repo/ICreateRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBanking.Models.Contract.Repo
+{
+    interface ICreateRepository<T>
+    {
+        Task<IEntity> CreateAsync(IEntity entity);
+
+    }
+}
---
+++ OnlineBanking/Models/Contract/Repo/IReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBanking.Models.Contract.Repo
+{
+    interface IReadRepository<T>
+    {
+        Task<IEntity> GetByIdAsync(int id);
+        Task<IEnumerable<IEntity>> GetAllAsync(Func<T, bool> predicate = null);
+    }
+}
